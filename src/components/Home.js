13a98a4c77d07c030af636a13fbb95436186d618
Donald Dominic <gotodonald@gmail.com>
2023-06-22 23:20:40 +0100

    Add initial home page - Add Homepage component - Add initial styles - Refactor index page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/Home.module.css'
+/**
+ * Renders Scoop home page
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <h1>SCOOP</h1>
+        </div>
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import Head from "next/head";
-import Script from "next/script"
 
-import styles from '@/styles/Home.module.css';
+import Home from "@/components/Home";
 
 export default function Index() {
 
 	return (
     <>
       <Head>
-        <title>Phaser App</title>
+        <title>Scoop</title>
         <meta
           name="description"
           content="Scoop voice and video caller"
@@ -16,13 +15,9 @@ export default function Index() {
         <meta name="viewport" content="width=device-width, initial-scale=1 maximum-scale = 1.0, minimum-scale = 1.0, user-scalable = 0, minimal-ui" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-			{/*<Script*/}
-			{/*	src="/scripts/phaser.min.js"*/}
-			{/*	onError={() => console.log("on Error")}*/}
-			{/*	onLoad={() => console.log("on Load")}*/}
-			{/*	onReady={setup}*/}
-			{/*/>*/}
+        <div>
+            <Home />
+        </div>
 	</>
 	);
 }
